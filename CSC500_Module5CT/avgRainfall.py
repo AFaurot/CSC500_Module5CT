9294c8ff203c9ce5ea134ddd9894c1d22b5ffe1c
AFaurot <aaron.faurot@csuglobal.edu>
2024-07-13 23:39:54 -0600

    avgRainfall.py initial commit

--- CSC500_Module5CT/avgRainfall.py
+++ CSC500_Module5CT/avgRainfall.py
@@ -1,21 +1,34 @@
 def main () :
 
+    #num_years variable used later in outer for loop
     num_years = int(input("Enter how many years to check for average rainfall: "))
+    print()
+    #List structure to store month names.
     months_list = ["Jan", "Feb", "Mar", "Apr", "May", "June",
                    "July", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    #total_inches_rain updated later in the inner for loop
     total_inches_rain = float(0)
+    #since program only asks for number of years, months = years * 12
     total_months = num_years * 12
+    #outer loop
     for i in range(num_years) :
+        #year_label used later in input prompt
         year_label = i + 1
+        #month_incrementer used to increment position of months_list
         month_incrementer = 0
+        #inner loop
         for i in range(12) :
-            rain=float(input("Enter total rain for the month of {} in year {}: "
+            #store month_rain value based on user input
+            month_rain=float(input("Enter total rain for the month of {} in year {}: "
                              .format(months_list[month_incrementer], year_label)))
             month_incrementer +=1
-            total_inches_rain = round((total_inches_rain + rain), 4)
-    print("Total rain over a course of {} months = {} inches"
+            #add month_rain to current value of total_inches_rain, round to 4 decimal places
+            total_inches_rain = round((total_inches_rain + month_rain), 4)
+    #print total inches of rain over total_months
+    print("\nTotal rain over a course of {} months = {} inches"
           .format(total_months, total_inches_rain))
-    print("Average rainfall over the entire period =",
+    #calculate average, format and print output.
+    print("Average monthly rainfall over the entire period =",
           round((total_inches_rain / total_months), 4), "inches")
 
 
--- CSC500_Module5CT/bookstorePoints.py
+++ CSC500_Module5CT/bookstorePoints.py
@@ -1,3 +1,21 @@
 def main () :
 
+    #num_books purchased will determine points awarded
+    num_books = int(input("Enter the number of books purchased this month : "))
+    #points variable to store number of points
+    points = 0
+    #Print number of points awarded based on user input
+    if num_books < 2 :
+        points = 0
+    elif num_books >=2 and num_books < 4 :
+        points = 5
+    elif num_books >=4 and num_books < 6 :
+        points = 15
+    elif num_books >=6 and num_books < 8 :
+        points = 30
+    elif num_books >= 8 :
+        points = 60
+    #print points earned
+    print("You have earned {} book club points".format(points))
+
 if __name__ == '__main__': main()
